fe3788fd3172919adfa12663e232f33e2594b612
Harold <haroldcj95@gmail.com>
2019-02-27 23:28:59 -0400

    refactorizar ventas.js

--- test/ventas_test.js
+++ test/ventas_test.js
@@ -2,17 +2,33 @@ var assert = require('assert');
 var expect = require('chai').expect;
 var should = require('chai').should();
 
-var departamentoVentas= require('../ventas.js');
+var departamentoVentas = require('../ventas.js');
+describe('Venta', function () {
+    let venta;
 
-describe('Venta',function(){
-    it('total de venta sin productos deberia devolver 0',function(){
-        let venta=new departamentoVentas.Venta();
-        expect(venta.total()).equal(0);
+    beforeEach(function () {
+        venta = new departamentoVentas.Venta();
     });
-    it('total de venta con un producto deberia devolver',function(){
-        let venta=new departamentoVentas.Venta();
-        let mantenimiento= new departamentoVentas.Servicio("Mantenimiento",115);
-        venta.agregarAlCarrito(mantenimiento,1);
-        expect(venta.total()).equal(115);
+
+    it('calcularTotal de venta sin productos deberia devolver 0', function () {
+        expect(venta.calcularTotal()).equal(0);
+    });
+    it('calcularTotal de venta con un servicio deberia devolver', function () {
+        let mantenimiento = new departamentoVentas.Servicio("Mantenimiento", 115);
+        venta.agregarAlCarrito(mantenimiento, 1);
+        expect(venta.calcularTotal()).equal(115);
+    });
+    it('calcularTotal de venta con un producto deberia devolver', function () {
+        let monitor = new departamentoVentas.Servicio("Monitor32Pulgadas", 400);
+        venta.agregarAlCarrito(monitor, 1);
+        expect(venta.calcularTotal()).equal(400);
+    });
+
+    it('calcularTotal de venta con un producto y un servicio deberia devolver', function () {
+        let mantenimiento = new departamentoVentas.Servicio("Mantenimiento", 115);
+        let monitor = new departamentoVentas.Producto("Monitor32Pulgadas", 400);
+        venta.agregarAlCarrito(mantenimiento, 1);
+        venta.agregarAlCarrito(monitor, 1);
+        expect(venta.calcularTotal()).equal(515);
     });
 });
--- ventas.js
+++ ventas.js
@@ -1,49 +1,57 @@
-
-class Venta{
-    constructor(){
-        this.carritoDeCompras=new Array();
+class Venta {
+    constructor() {
+        this.carritoDeCompras = new Array();
     }
 
-    agregarAlCarrito(producto,cantidad){
-        let venta={
-            prod:producto,
-            cant:cantidad
+    agregarAlCarrito(producto, cantidad) {
+        let venta = {
+            prod: producto,
+            cant: cantidad
         };
         this.carritoDeCompras.push(venta);
     }
- 
-    total(){
-        let total=0;
-        if(this.carritoDeCompras.length>0){
-        this.carritoDeCompras.forEach(function(item){
-            const producto=item.prod;
-            const cantidad=item.cant;
-            total+=producto.precio*cantidad;
+
+    calcularTotal() {
+        let calcularTotal = 0;
+        this.carritoDeCompras.forEach(function (item) {
+            const producto = item.prod;
+            const cantidad = item.cant;
+            calcularTotal += producto.precio * cantidad;
         });
-        }
-        return total;
-    }
-
-}
-
-class Producto{
-    constructor(nombre,precio){
-        this.nombre=nombre;
-        this.precio=precio;
+        return calcularTotal;
     }
 }
 
-class Servicio extends Producto{
-    constructor(nombre,precio){
-        super(nombre,precio);
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    estaDisponible(cantidad) {
+        if (cantidad > 999)
+            return false;
+        return true;
+    }
+    calcularTarifa(cantidad) {
+        return (this.precio * cantidad);
     }
 }
 
-
-class Articulo extends Producto{
-    constructor(nombre,precio){
-        super(nombre,precio);
+class Servicio extends Producto {
+    constructor(nombre, precio) {
+        super(nombre, precio);
     }
 }
 
-module.exports =  { Venta, Servicio, Producto, Articulo }
+class Articulo extends Producto {
+    constructor(nombre, precio) {
+        super(nombre, precio);
+    }
+}
+
+module.exports = {
+    Venta,
+    Servicio,
+    Producto,
+    Articulo
+}
